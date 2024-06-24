21ab3a78f1149a164382750781e20c5ab3f4c369
sfall <sfall3003@gmail.com>
2024-06-24 22:23:17 +0600

    - Изменение типов сущности Report

--- src/main/java/pro/sky/telegrambot/model/ReportModel.java
+++ src/main/java/pro/sky/telegrambot/model/ReportModel.java
@@ -9,16 +9,22 @@ public class ReportModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "report_date")
     private LocalDateTime reportDate;
+
     @Column(name = "client_id")
     private Integer clientId;
+
     @Column(name = "pet_id")
     private Long petId;
+
     @Column(name = "pet_photo_path")
     private String petPhotoPath;
+
     @Column(name = "pet_info")
     private String petInfo;
+
     @Column(name = "is_accepted")
     private boolean isAccepted;
 
