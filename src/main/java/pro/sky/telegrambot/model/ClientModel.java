6a0e6e01858ccfa91c6aec470eadfca710ff0e25
Poteryashka163 <scrat0071@gmail.com>
2024-07-07 21:56:40 +0400

    Исправил офибку при котором не работало Menu. В мектоде formatPhoneNumber переделал неправильные формиаты номеров.

--- src/main/java/pro/sky/telegrambot/model/ClientModel.java
+++ src/main/java/pro/sky/telegrambot/model/ClientModel.java
@@ -29,7 +29,7 @@ public class ClientModel {
     private Integer pet_id ;
 
     public ClientModel(){
-    };
+    }
 
 
 
--- src/main/java/pro/sky/telegrambot/service/impl/PhoneNumberValidatorImpl.java
+++ src/main/java/pro/sky/telegrambot/service/impl/PhoneNumberValidatorImpl.java
@@ -7,9 +7,10 @@ import pro.sky.telegrambot.service.ClientService;
 import java.util.regex.Pattern;
 @Service
 public class PhoneNumberValidatorImpl {
-    private static final String PHONE_PATTERN = "^(\\+7-9\\d{2}-\\d{3}-\\d{2}-\\d{2}|8-9\\d{2}-\\d{3}-\\d{2}-\\d{2}|89\\d{9}|\\+79\\d{9}) - (.+)$";
+    //Шаблон для проверки строк на соответствие формату номера телефона и имени клиента.
+    private static final String PHONE_PATTERN = "^(\\+7-9\\d{2}-\\d{3}-\\d{2}-\\d{2}|8-9\\d{2}-\\d{3}-\\d{2}-\\d{2}|89\\d{9}|\\+79\\d{9}|79\\d{9}) - (.+)$";
     private static final Pattern pattern = Pattern.compile(PHONE_PATTERN);
-    // Метод для проверки правильности формата номера телефона и имени
+    // Метод для проверки правильности формата номера телефона и имени.
     public static boolean isValid(String massage) {
         return pattern.matcher(massage).matches();
     }
@@ -19,21 +20,30 @@ public class PhoneNumberValidatorImpl {
         phoneNumber = phoneNumber.replaceAll("\\D", "");
         // Приводим номер к формату +7-9**-***-**-**
         if (phoneNumber.matches("^79\\d{9}$")) {
-            return phoneNumber.replaceFirst("(\\+7)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "$1-$2-$3-$4-$5");
+            return phoneNumber.replaceFirst("(79)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+7-$1-$2-$3-$4");
         } else if (phoneNumber.matches("^\\+79\\d{9}$")) {
-            return phoneNumber.replaceFirst("(\\+7)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "$1-$2-$3-$4-$5");
+            return phoneNumber.replaceFirst("(\\+79)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "$1-$2-$3-$4-$5");
         } else if (phoneNumber.matches("^89\\d{9}$")) {
-            return phoneNumber.replaceFirst("(\\+7)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "$1-$2-$3-$4-$5");
+            return phoneNumber.replaceFirst("(89)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+7-$1-$2-$3-$4");
+        } else if (phoneNumber.matches("^8-9\\d{9}$")) {
+            return phoneNumber.replaceFirst("(8-9)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+7-$1-$2-$3-$4");
+        } else if (phoneNumber.matches("^\\+7-9\\d{9}$")) {
+            return phoneNumber.replaceFirst("(\\+7-9)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "$1-$2-$3-$4-$5");
+        } else if (phoneNumber.matches("^7-9\\d{9}$")) {
+            return phoneNumber.replaceFirst("(7-9)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+7-$1-$2-$3-$4");
         }
         // Если номер не соответствует ни одному из форматов, выбрасываем исключение
         throw new IllegalArgumentException("Invalid phone number format: " + phoneNumber);
     }
-
+    //Метод для сохранения клиета в базе данных.
     public static ClientModel saveClient(String message, ClientService clientService) {
+        // Разделение сообщения на части
         String[] parts = message.split(" - ", 2);
         String phoneNumber = parts[0].trim();
         String name = parts[1].trim();
+        // Форматирование телефонного номера
         phoneNumber = formatPhoneNumber(phoneNumber);
+        // Создание нового объекта клиента
         ClientModel client = new ClientModel();
         client.setPhone(phoneNumber);
         client.setName(name);
